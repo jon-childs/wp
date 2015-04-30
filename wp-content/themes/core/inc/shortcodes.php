4b1547641f61e2a62c8306a770b25f839894c886
Jon Childs <jon.g.childs@gmail.com>
2015-04-29 23:42:44 -0600

    fsm to jc_core

--- wp-content/themes/core/inc/shortcodes.php
+++ wp-content/themes/core/inc/shortcodes.php
@@ -5,38 +5,38 @@
  * [site-name]
  * Prints site name from theme customizer
  **/
-function fsm_shortcode_site_name($atts) {
-    return '<span class="site-name">' . get_theme_mod( 'fsm_site_site_name', 'Your Site Name Here') . '</span>';
+function jc_core_shortcode_site_name($atts) {
+    return '<span class="site-name">' . get_theme_mod( 'jc_core_site_site_name', 'Your Site Name Here') . '</span>';
 }
-add_shortcode( 'site-name', 'fsm_shortcode_site_name');
+add_shortcode( 'site-name', 'jc_core_shortcode_site_name');
 
 /**
  * [copyright-name]
  * Prints copyright name from theme customizer
  **/
-function fsm_shortcode_copyright_name($atts) {
-    return '<span class="copyright-name">' . get_theme_mod( 'fsm_site_copyright_name', 'Your Copyright Name Here') . '</span>';
+function jc_core_shortcode_copyright_name($atts) {
+    return '<span class="copyright-name">' . get_theme_mod( 'jc_core_site_copyright_name', 'Your Copyright Name Here') . '</span>';
 }
-add_shortcode( 'copyright-name', 'fsm_shortcode_copyright_name');
+add_shortcode( 'copyright-name', 'jc_core_shortcode_copyright_name');
 
 /**
  * [site-logo]
  * Prints site logo from theme customizer
  **/
-function fsm_shortcode_site_logo($atts) {
-    if ( get_theme_mod( 'fsm_site_site_logo' ) !== "" ) {
-        return '<img src="' . get_theme_mod( 'fsm_site_site_logo' ) . '" alt="' . get_theme_mod( 'fsm_site_site_name', 'Your Site Name Here') . '" class="logo" />';
+function jc_core_shortcode_site_logo($atts) {
+    if ( get_theme_mod( 'jc_core_site_site_logo' ) !== "" ) {
+        return '<img src="' . get_theme_mod( 'jc_core_site_site_logo' ) . '" alt="' . get_theme_mod( 'jc_core_site_site_name', 'Your Site Name Here') . '" class="logo" />';
     } else {
-        return '<img src="' . get_template_directory_uri() . '/img/logo-dark.png' . '" alt="' . get_theme_mod( 'fsm_site_site_name', 'Your Site Name Here') . '" class="logo" />';
+        return '<img src="' . get_template_directory_uri() . '/img/logo-dark.png' . '" alt="' . get_theme_mod( 'jc_core_site_site_name', 'Your Site Name Here') . '" class="logo" />';
     }
 }
-add_shortcode( 'site-logo', 'fsm_shortcode_site_logo');
+add_shortcode( 'site-logo', 'jc_core_shortcode_site_logo');
 
 /**
  * [list-sibling-pages]
  * Renders a list of sibling pages of the current page, as determined by page 'Parent' attribute
  **/
-function fsm_list_sibling_pages(){
+function jc_core_list_sibling_pages(){
     $siblings = wp_list_pages( array(
         'echo'  => false,
         'title_li'	=> '',
@@ -46,6 +46,6 @@ function fsm_list_sibling_pages(){
     ) );
     return '<nav><ul class="sibling-pages">'.$siblings.'</ul></nav>';
 }
-add_shortcode( 'list-sibling-pages', 'fsm_list_sibling_pages');
+add_shortcode( 'list-sibling-pages', 'jc_core_list_sibling_pages');
 
 ?>
